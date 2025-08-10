c064d86e85f0a82f6aa65ec5dc792f3880b4b9d8
Tomchuck07 <szymon.tomczak7@icloud.com>
2025-08-10 15:43:56 +0200

    First two lessons (#1)
    * initial commit
    * loc update
    * add: initial .gitignore file
    * update .gitignore
    * update env config files
    * deleted not needed main.py file
    * first two lessons

---
+++ series_intro.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+series = {
+    'year':[1978,1999,2008,2013],
+    'champion':['milan','udienese', 'juventus', 'roma'],
+    'name':'team'
+}
+
+def get(series, year):
+    ins_inx = series['year'].index(year)
+    return series['champion'][ins_inx]
+
+
+get(series, 2013)
+
+#why do we prefer to use pandas instead
+
+profit = pd.Series([205,199,10,-30], name = 'money')
+profit
+
+rev = pd.Series([205,199,10,-30], name = 'money', dtype='int64[pyarrow]')
+rev
+
+rev.index
+
+class Foo:
+    pass
+
+sW_objVls = pd.Series(
+    ['John','Weronika',13,Foo()],
+    index = ['man','women','day','function'],
+    name = 'example')
+
+sW_objVls
+
+
+import numpy as np
+
+ex2=  pd.Series([23,21,np.nan,23,43,2.5,23])
+ex2.size
+print(ex2.count())
+
+sW_objVls[2]
