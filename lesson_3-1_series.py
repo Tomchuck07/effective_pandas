cc7cb24e6b9a0c990ae2ea8ebb0ea0a1d5be6da9
Tomchuck07 <szymon.tomczak7@icloud.com>
2025-08-12 10:37:04 +0200

    cd. nauki seriesow (#8)

--- lesson_3-1_series.py
+++ lesson_3-1_series.py
@@ -41,4 +41,70 @@ ex2=  pd.Series([23,  21,  np.nan,  23,  43,  2.5,  23])
 ex2.size
 print(ex2.count())
 
-sW_objVls[2]
+
+#numpy array vs padnas series
+
+npA_songs = np.array([145, 142, 38, 13])
+
+pdS_songs = pd.Series([145, 142, 38, 13],
+                      index = ['Paul', 'John', 'George', 'Ringo'],
+                      name = 'counts',
+                      dtype='int64[pyarrow]')
+
+
+npA_songs[1]
+pdS_songs[1] #future warning, use .iloc[pos]
+pdS_songs.iloc[1]
+
+npA_songs.mean()
+pdS_songs.mean()
+
+len(set(dir(npA_songs)) & set(dir(pdS_songs)))
+
+#cd., maski
+
+mask = pdS_songs > pdS_songs.median() #boolean array
+mask
+
+pdS_songs[mask]
+pdS_songs[pdS_songs>pdS_songs.median()]
+
+npA_songs[npA_songs>np.median(npA_songs)]
+
+#Categorical Data
+
+
+s = pd.Series(['s', 'm', 'l'], dtype = 'category')
+s.cat.ordered
+
+s2 = pd.Series(['m', 'l', 'xs', 's', 'xxl'], dtype = 'string[pyarrow]')
+sizes_type = pd.CategoricalDtype(categories=['s', 'm', 'l'], ordered = True)
+s3 = s2.astype(sizes_type)
+s3
+s3.sort_values()
+s3>'s'
+
+s = pd.Series(['s','m','l'], dtype = 'category')
+s.cat.reorder_categories(['xl', 'l', 'm', 's', 'xs'], ordered=True) #ValueError - categories differ
+s.cat.categories
+
+
+s.cat.add_categories(['xs','xl']).cat.reorder_categories(['xl', 'l', 'm', 's', 'xs'], ordered=True)
+
+s3.str.upper()
+
+#Exercises
+#1. Using Jupyter, create a series with the temperature values for the last seven days. Filter out the values below the mean.
+#2. Using Jupyter, create a series with your favorite colors. Use a categorical type.
+
+#1.
+temperatures = pd.Series(['23.5', '25.3', '18.1', '14.0', '26.3', '32.4', '33.1'],
+                         index=['tuesday', 'wendesday', 'thursday', 'friday', 'saturday', 'sunday', 'moonday'],
+                         dtype = 'double[pyarrow]',
+                         name = 'celsius') 
+temperatures[temperatures >= temperatures.mean()]
+
+#2.
+color_types = pd.CategoricalDtype(['blue', 'green', 'purple', 'white', 'black', 'pink', 'orange', 'red'], ordered=True)
+colors = pd.Series(['red', 'blue', 'green'], dtype=color_types)
+colors.sort_values()
